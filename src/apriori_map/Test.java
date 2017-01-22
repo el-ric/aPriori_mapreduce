9886357fd0c19e67e8efca7a9b842d3045600963
Unknown <spiderkev@gmail.com>
2017-01-22 20:54:53 +0000

    Fixed Frequent Items issues.

--- src/apriori_map/Test.java
+++ src/apriori_map/Test.java
@@ -2,9 +2,12 @@ package apriori_map;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.*;
 
 public class Test {
 	public static String inputFile = "data_for_project.txt";
+	public static String itemSet = "A,B,C,D";
+	public static ArrayList<String[]> associcationRules;
 
 	private static boolean isItemInBasket(String items, String basket) {
 		String[] itemSet = items.split(",");
@@ -22,6 +25,60 @@ public class Test {
 		}
 		
 		return isPresent;
+	}
+	
+	private static String getAssociation(String itemset, String basket) {
+		String[] items  = itemset.split(",");
+		String[] basketItems = basket.split(",");
+		String remainingItems = new String();
+		
+		remainingItems = "";
+		for(int i=0; i<basketItems.length; i++) {
+			boolean inItemset = false;
+			for(int j=0; j<items.length; j++) {
+				if(basketItems[i].equals(items[j]))
+					inItemset = true;
+			}
+			if(!inItemset) {
+				if(remainingItems.equals(""))
+					remainingItems = basketItems[i];
+				else
+					remainingItems = remainingItems + "," + basketItems[i];
+			}
+		}
+		
+		String[] sorted = remainingItems.split(",");
+		Arrays.sort(sorted);
+		remainingItems = String.join(",", sorted);
+		
+		return remainingItems;
+	}
+	
+	public static ArrayList<String[]> getCombinations(String string, int size) {
+		ArrayList<String[]> assRules = new ArrayList<String[]>();
+		String[] items  = string.split(",");
+		
+		for (int i=0; i+size<=items.length; i++) {
+			for(int j=i+1; j<items.length; j++) {
+				String comb = new String();
+				comb = items[i];
+				for(int k=0; k<size-1; k++)
+					if(j+k < items.length) {
+						comb = comb + "," + items[j+k];
+					}
+					else if(i+k+size <= items.length) {
+						comb = comb + "," + items[k];
+					}
+				if(comb.length() == 2*size-1) {
+					String associatedItems = getAssociation(comb, string);
+					String[] association = new String[2];
+					association[0] = comb;
+					association[1] = associatedItems;
+					assRules.add(association);
+				}
+			}
+		}
+		return assRules;
 	}
 	
 	private static int checkResults(String string) {
@@ -51,7 +108,14 @@ public class Test {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(checkResults("butter,other vegetables,whole milk"));
+		//System.out.println(checkResults("butter,other vegetables,whole milk"));
+		ArrayList<String[]> assRules = new ArrayList<String[]>();
+		assRules = getCombinations("A,B,C,D", 2);
+		
+		for(int i=0; i<assRules.size(); i++) {
+			System.out.println(assRules.get(i)[0] + " -> " + assRules.get(i)[1]);
+		}
+		
 	}
 
 }
--- src/apriori_map/apriori.java
+++ src/apriori_map/apriori.java
@@ -15,16 +15,26 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.util.*;
 
 public class apriori {
+	// USER INPUT (Apriori Variables)
+	public static double confidence = 0.4;
+	public static int supportFrequency = 100;
+	//public static double supportThreshold = 0.1; // Frequency as a fraction
+		
+		
+	// INPUT-OUTPUT FOLDERS
 	public static String inputFile = "data_for_project.txt";
 	public static String outputFolder = "mapreduce";
+	
+	// GLOBAL VARIABLES
 	public static String outputFile;
-	public static double supportThreshold = 0.1;
-	public static int supportFrequency = 80;
 	public static int total_read[] = new int[50];
 	public static int passNumber;
 	public static String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+	
+	// DATA STRUCTURES
 	public static ArrayList<String> freqItems, candidateItems;
 	public static ArrayList<Map<String, Integer>> allFrequentItemsets = new ArrayList<Map<String, Integer>>();
+	public static ArrayList<String[]> associationRules = new ArrayList<String[]>(); //Format: (String[0] = A, String[1] = B, String[2] = C) => A -> B, confidence C
     
 	private static boolean isItemInBasket(String items, String basket) {
 		String[] itemSet = items.split(",");
@@ -118,7 +128,7 @@ public class apriori {
 			return false;
 		*/
 		
-		if (sum>supportFrequency)
+		if (sum>=supportFrequency)
 			return true;
 		else
 			return false;
@@ -194,11 +204,146 @@ public class apriori {
 		catch (IOException | ClassNotFoundException | InterruptedException e) {
 			e.printStackTrace();
 		} 
-	    
+
 		return success;
+	}
+
+	private static String getAssociation(String itemset, String basket) {
+		String[] items  = itemset.split(",");
+		String[] basketItems = basket.split(",");
+		String remainingItems = new String();
+		
+		remainingItems = "";
+		for(int i=0; i<basketItems.length; i++) {
+			boolean inItemset = false;
+			for(int j=0; j<items.length; j++) {
+				if(basketItems[i].equals(items[j]))
+					inItemset = true;
+			}
+			if(!inItemset) {
+				if(remainingItems.equals(""))
+					remainingItems = basketItems[i];
+				else
+					remainingItems = remainingItems + "," + basketItems[i];
+			}
+		}
+	
+		remainingItems = combineStrings(remainingItems.split(",")); // Sort string in ascending order
+		
+		return remainingItems;
+	}
+	
+	// Returns all combinations in an array list of 2D Strings containing left and right part of association rules
+	public static ArrayList<String[]> getCombinations(String string, int size) {
+		ArrayList<String[]> assRules = new ArrayList<String[]>();
+		String[] items  = string.split(",");
+		
+		for (int i=0; i+size<=items.length; i++) {
+			for(int j=i+1; j<items.length; j++) {
+				String comb = new String();
+				comb = items[i];
+				for(int k=0; k<size-1; k++)
+					if(j+k < items.length) {
+						comb = comb + "," + items[j+k];
+					}
+					else if(i+k+size <= items.length) {
+						comb = comb + "," + items[k];
+					}
+				if(comb.length() == 2*size-1) {
+					String associatedItems = getAssociation(comb, string);
+					String[] association = new String[2];
+					association[0] = comb;
+					association[1] = associatedItems;
+					assRules.add(association);
+				}
+			}
+		}
+		return assRules;
+	}
+	
+	private static void findRulesForPairs() {
+		Map itemset = new HashMap<String, Integer>();
+		Map freqItems = new HashMap<String, Integer>();
+		itemset = allFrequentItemsets.get(1);
+		freqItems = allFrequentItemsets.get(0);
+		
+	    Iterator it = itemset.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        String[] items = pair.getKey().toString().split(",");
+	        
+	        int supportAandB = pair.getValue().hashCode();
+	        int supportA = freqItems.get(items[0]).hashCode();
+
+	        double c = 1.0 * supportAandB/supportA;
+	        
+	        //Add rule if it meets confidence criteria
+	        if(c>=confidence)
+	        	addRule(items[0], items[1], c);
+	        
+	        // Check reverse association
+	        supportA = freqItems.get(items[1]).hashCode();
+	        c = 1.0 * supportAandB/supportA;
+	        
+	        if(c>=confidence)
+	        	addRule(items[1], items[0], c);
+
+	        it.remove(); // avoids a ConcurrentModificationException
+	    }
+			
+	}
+	
+	private static void addRule(String a, String b, double c) {
+		String[] rule = new String[3];
+    	rule[0] = a;
+    	rule[1] = b;
+    	rule[2] = Double.toString(c);
+    	associationRules.add(rule);	 
+	}
+	
+	private static void findRulesForTriples() {
+		Map itemset = new HashMap<String, Integer>();
+		Map freqPairs = new HashMap<String, Integer>();
+		itemset = allFrequentItemsets.get(2);
+		freqPairs = allFrequentItemsets.get(1);		
+		
+		Iterator it = itemset.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry line = (Map.Entry)it.next();
+	        String triple = line.getKey().toString();
+	        ArrayList<String[]> potentialRules = getCombinations(triple, 2);
+	        
+	        for(int i=0; i<potentialRules.size(); i++) {
+	        	String a = potentialRules.get(i)[0];
+	        	String b = potentialRules.get(i)[0];
+	        	int supportAandB = line.getValue().hashCode();
+		        int supportA = freqPairs.get(a).hashCode();
+		        double c = 1.0 * supportAandB/supportA;
+		        System.out.println("Item: " + a + " -> " + b + "Sup A and B " + supportAandB + " Support A " + supportA);
+		        if(c>=confidence)
+		        	addRule(a, b, c);
+	        }
+	    }
+	}
+	
+	public static void displayAssociationRules() {
+		System.out.println("\n==== Association Rules ====\n");
+		for(int i=0; i<associationRules.size(); i++) {
+			String[] rule = associationRules.get(i);
+			System.out.println(rule[0] + " -> " + rule[1] + "\t" + rule[2]);
+		}
+	}
+	
+	private static void findAssociationRules() {
+		if(allFrequentItemsets.size() > 1)
+			findRulesForPairs();
+		if(allFrequentItemsets.size() > 2)
+			findRulesForTriples();
 	}
 	
 	public static void main(String[] args) throws InterruptedException, ClassNotFoundException {
 		findFrequentItems();
+		findAssociationRules();
+		displayAssociationRules();
 	}
 }
